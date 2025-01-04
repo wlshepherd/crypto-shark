4b018c80f39d4e7fd064fd43dbb36ef319f1c6d6
William Shepherd <will.shepherdy.2002@gmail.com>
2025-01-04 18:18:02 +0000

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/crypto-shark">
       <App />
     </BrowserRouter>
   </React.StrictMode>
